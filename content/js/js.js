d402d5c633e386bea9fd64df79dddd204d8a93a8
Daniel Krogh Andersen <daniel@kroghkommunikation.dk>
2020-11-18 12:21:14 +0100

    .

--- content/js/js.js
+++ content/js/js.js
@@ -1,3 +1,7 @@
+/*
+* Controller
+*/
+// Fetch af 17 verdensmål
 async function fetchGoalsData() {
     let response = await fetch('https://api.mediehuset.net/sdg/goals');
     let data = await response.json();
@@ -5,6 +9,7 @@ async function fetchGoalsData() {
     return data.items;
 };
 
+// Håndtering af 17 verdensmål
 async function handleGoalData() {
     let goalsData = [...await fetchGoalsData()];
     let sortedGoalsData = []
@@ -18,6 +23,7 @@ async function handleGoalData() {
     return sortedGoalsData;
 };
 
+// Fetch af specifik verdensmål udfra id
 async function fetchSpecificGoalData(id) {
     let response = await fetch(`https://api.mediehuset.net/sdg/goals/${id}`);
     let data = await response.json();
@@ -27,12 +33,13 @@ async function fetchSpecificGoalData(id) {
     return dataArray;
 };
 
+// Håndtering af specifik verdensmål udfra id
 async function handleSpecificGoalData(id) {
     let specificGoalData = [...await fetchSpecificGoalData(id)];
     let sortedSpecificGoalData = []
 
     specificGoalData.forEach(specificGoal => {
-        let specificData = [specificGoal.description, `#${specificGoal.color}`];
+        let specificData = [specificGoal.description, `#${specificGoal.color}`, specificGoal.targets];
 
         sortedSpecificGoalData.push(specificData);
     })
@@ -40,38 +47,54 @@ async function handleSpecificGoalData(id) {
     return sortedSpecificGoalData;
 };
 
+/*
+* View
+*/
 async function setGoalView() {
     let goalData = [...await handleGoalData()];
 
-    const goalsContainer = document.querySelector('#goals-container');
+    const goalsContainer = document.querySelector('#goals-container'); // Container der skal indeholde alle mål
 
-    goalData.forEach(goal => {
+    goalData.forEach(goal => { // For hvert mål laves HTML som tilføjes til vores container
         let imgHTML =
             `<div class="goal" data-id="${goal[3]}" style="background-color:${goal[2]}">
-                <h2>${goal[0]}</h2>
-                ${goal[1]}
+                <div>
+                    <h2>${goal[0]}</h2>
+                    ${goal[1]}
+                </div>
             </div>`;
 
         goalsContainer.insertAdjacentHTML('beforeend', imgHTML);
     })
 
-    let htmlGoals = document.querySelectorAll('.goal');
+    let htmlGoals = document.querySelectorAll('.goal'); // Array med alle de oprettede mål
 
-    htmlGoals.forEach(htmlGoal => htmlGoal.addEventListener('click', () => {
+    htmlGoals.forEach(htmlGoal => htmlGoal.addEventListener('click', () => { // Ved klik på et af de oprettede mål
+        setSpecificGoalView(htmlGoal.getAttribute('data-id')); // Kalder funktion med id fra klikket mål
+
         async function setSpecificGoalView(id) {
-            let specificGoalData = [...await handleSpecificGoalData(id)];
+            let specificGoalData = [...await handleSpecificGoalData(id)]; // Får fat i data ang. klikket mål
 
-            let html =
+            let html = // Overlay med målets baggrundsfarve og beskrivelse
             `<div id="overlay" style="background-color:${specificGoalData[0][1]}">
                 <div class="container">
+                    <h1>Beskrivelse af mål ${id}</h1>
                     <p>${specificGoalData[0][0]}</p>
                 </div>
             </div>`;
 
-            document.body.insertAdjacentHTML('beforeend', html)
+            document.body.insertAdjacentHTML('beforeend', html); // Overlay tilføjes i bunden af body
+
+            specificGoalData[0][2].forEach(subGoal => { // For hvert undermål laves html som tilføjes til overlay container
+                let subGoalHTML = `
+                <div class="sub-goal">
+                    <h2>${subGoal.title}</h2>
+                    <p>${subGoal.description}</p>
+                <div>`;
+
+                document.querySelector('#overlay .container').insertAdjacentHTML('beforeend', subGoalHTML)
+            })
         }
-        
-        setSpecificGoalView(htmlGoal.getAttribute('data-id'));
     }))
 
     document.body.addEventListener('click', () => {
